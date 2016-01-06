1aed4f9fc7ff4de6d3c937f40b1d136bff48d689
Young Foo <fuymsn@hotmail.com>
2016-01-06 19:16:30 +0800

    update gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var uglifycss = require('gulp-uglifycss');
 var rename = require('gulp-rename');
 
-gulp.task('default', function(){
+gulp.task('css-min', function(){
     gulp.src('./src/*.css')
         .pipe(uglifycss({
             //"max-line-len": 80
@@ -12,3 +12,11 @@ gulp.task('default', function(){
         }))
         .pipe(gulp.dest('./build/'));
 });
+
+gulp.task('watch', function(){
+    var watcher = gulp.watch('./src/*.css', ['css-min']);
+    watcher.on('change', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
